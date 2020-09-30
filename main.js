f0eeb0dff0fe918d1a55ec9d8ddcb020865ee1e8
borys25ol <oleynik.boris@gmail.com>
2020-09-30 11:38:20 +0300

    Add docstrings. Add whitelist url.

--- main.js
+++ main.js
@@ -21,10 +21,13 @@ const youtubeSelectors = {
 }
 
 // Extract source from location.
-const source = sources.find((source) =>
-  window.location.pathname.toLowerCase().includes(source)
-)
+const source = sources.find((source) => window.location.pathname.toLowerCase().includes(source))
 
+/**
+ * Extension entry point.
+ * Check current `source`, extract all data.
+ * Copy result JSON to buffer.
+ */
 ;(function main() {
   let rows
   let header
@@ -38,9 +41,15 @@ const source = sources.find((source) =>
   }
 
   // Make copy to buffer.
-  copy(JSON.stringify(getData(rows)))
+  copy(JSON.stringify(getData(rows, header)))
 })()
 
+/**
+ * Extract data from table depends on `source`.
+ * @param rows List with table rows selectors.
+ * @param header SocialBlade block with info about source.
+ * @return {[]} List with scraped objects from page.
+ */
 function getData(rows, header) {
   const results = []
 
@@ -59,36 +68,35 @@ function getData(rows, header) {
     }
   }
 
+  console.log(results)
+
   return results
 }
 
+/**
+ * Extract data for SocialBlade Instagram source.
+ * @param row Table row selector.
+ * @param header SocialBlade block with info about source.
+ * @return {Object} Object with scraped data.
+ */
 function getInstagramData(row, header) {
   const date = row.querySelector(instagramSelectors.date).textContent
 
   // Clean up subscribers count.
-  const rawSubscribers = row
-    .querySelector(instagramSelectors.subscribers)
-    .textContent.trim()
+  const rawSubscribers = row.querySelector(instagramSelectors.subscribers).textContent.trim()
   const intSubscribers = +rawSubscribers.replace(',', '')
 
-  const currentBrand = brands.find((brand) =>
-    header.textContent.toLowerCase().includes(brand)
-  )
+  const currentBrand = brands.find((brand) => header.textContent.toLowerCase().includes(brand))
 
-  const mediaUploads = +document.querySelector(instagramSelectors.mediaUploads)
-    .textContent
+  const mediaUploads = +document.querySelector(instagramSelectors.mediaUploads).textContent
 
   // Clean up engagement.
-  const rawEngagementRate = document
-    .querySelector(instagramSelectors.engagementRate)
-    .textContent.trim()
-
-  const intEngagementRate = +rawEngagementRate.replace('%', '')
+  const rawEngagement = document.querySelector(instagramSelectors.engagementRate).textContent.trim()
+  const intEngagementRate = +rawEngagement.replace('%', '')
 
   const avgLikes = +document.querySelector(instagramSelectors.likes).textContent
 
-  const avgComments = +document.querySelector(instagramSelectors.comments)
-    .textContent
+  const avgComments = +document.querySelector(instagramSelectors.comments).textContent
 
   return {
     date: toTimestamp(date),
@@ -102,22 +110,23 @@ function getInstagramData(row, header) {
   }
 }
 
-function getYoutubeData(row) {
+/**
+ * Extract data for YouTube Instagram source.
+ * @param row Table row selector.
+ * @param header SocialBlade block with info about source.
+ * @return {Object} Object with scraped data.
+ */
+function getYoutubeData(row, header) {
   const date = row.querySelector(youtubeSelectors.date).textContent
 
-  const currentBrand = brands.find((brand) =>
-    header.textContent.toLowerCase().includes(brand)
-  )
+  const currentBrand = brands.find((brand) => header.textContent.toLowerCase().includes(brand))
 
   // Clean up views.
-  const rawVideoViews = row.querySelector(youtubeSelectors.videoViews)
-    .textContent
+  const rawVideoViews = row.querySelector(youtubeSelectors.videoViews).textContent
   const intVideoViews = rawVideoViews.replace(',', '')
 
   // Clean up subscribers count.
-  const rawSubscribers = document
-    .querySelector(youtubeSelectors.subscribers)
-    .textContent.trim()
+  const rawSubscribers = document.querySelector(youtubeSelectors.subscribers).textContent.trim()
   const intSubscribers = rawSubscribers.replace('K', '')
 
   return {
@@ -129,11 +138,23 @@ function getYoutubeData(row) {
   }
 }
 
+/**
+ * Convert scraped (str) date to numeric timestamp format.
+ * By default convert to milliseconds, but we need in seconds.
+ * So it need divide by 1000.
+ * @param strDate Raw date string from page.
+ * @return {number} Date in timestamp format in seconds.
+ */
 function toTimestamp(strDate) {
   const dateObj = Date.parse(strDate)
   return dateObj / 1000
 }
 
+/**
+ * Provide ability to copy data to buffer.
+ * @param text JSON object for copy.
+ * @return {boolean}
+ */
 function copy(text) {
   const input = document.createElement('textarea')
   input.innerHTML = text
--- popup.js
+++ popup.js
@@ -1,11 +1,9 @@
 const changeColor = document.getElementById('scrape-page')
 
-changeColor.style.backgroundColor = 'red'
-changeColor.setAttribute('value', 'red')
+const whiteListPattern = 'https://socialblade.com/*'
 
 changeColor.onclick = function (element) {
-  const color = element.target.value
-  chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
+  chrome.tabs.query({ active: true, currentWindow: true, url: whiteListPattern }, function (tabs) {
     chrome.tabs.executeScript(tabs[0].id, { file: './main.js' })
   })
 }
